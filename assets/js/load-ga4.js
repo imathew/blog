e91859761391f0dda417484672a24c3fa0cb8b36
Mathew Inkson <mathew.inkson@dpfem.tas.gov.au>
2023-08-10 13:51:14 +1000

    Removed photoswipe

--- assets/js/load-ga4.js
+++
@@ -1,7 +0,0 @@
-var doNotTrack = false;
-if (!doNotTrack) {
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
-	gtag('config', '{{ .Site.Params.googleAnalytics }}', { 'anonymize_ip': false });
-}
